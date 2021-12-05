cb15aaaa95ad47805985cd48f004e843116c8316
Samuel Gregson <55996456+Zacxalot@users.noreply.github.com>
2021-12-05 22:14:53 +0000

    Day 5 Part 2, slightly quicker, still slow.
    Slowest day to run yet! Hard to figure out why it's slow though...

--- src/days/day_5/day_5.rs
+++ src/days/day_5/day_5.rs
@@ -2,7 +2,6 @@ use std::fs;
 use std::time::Instant;
 
 
-
 use crate::Answer;
 
 #[derive(Debug)]
@@ -15,16 +14,18 @@ struct Line{
 
 pub fn day_5_main() -> Answer{
     let time_before = Instant::now();
+
+
     let mut lines:Vec<Line> = vec![];
     
     let mut max_x = 0;
     let mut max_y = 0;
 
     for line in fs::read_to_string("src/days/day_5/input1.txt").unwrap().split("\n"){
-        
+        // Split the line into a vec of 4 ints
         let vals = line.split(|c| c == ' ' || c == '-' || c == '>' || c == ',').map(|val|  val.trim()).filter(|c| c.len() > 0).map(|num| num.parse::<i32>().unwrap()).collect::<Vec<i32>>();
-        // let vals = line.split(|c| c == ' ' || c == '-' || c == '>' || c == ',').map(|val|  val.trim().parse::<i32>().unwrap()).collect::<Vec<i32>>();
 
+        // Convert vec to Line struct
         let new_line = Line{
             x1:*vals.get(0).unwrap(),
             y1:*vals.get(1).unwrap(),
@@ -32,6 +33,7 @@ pub fn day_5_main() -> Answer{
             y2:*vals.get(3).unwrap()
         }; 
 
+        // Find the bounds of the vent map
         if new_line.x1 > max_x {max_x = new_line.x1};
         if new_line.x2 > max_x {max_x = new_line.x2};
         if new_line.y1 > max_y {max_y = new_line.y1};
@@ -40,18 +42,27 @@ pub fn day_5_main() -> Answer{
         lines.push(new_line);
     }
 
+    
+    // Get two empty vent maps
     let mut vent_map_1 = (0..max_y+1).map(|_| vec![0;1 + max_x as usize]).collect::<Vec<Vec<i32>>>();
+    let mut vent_map_2 = (0..max_y+1).map(|_| vec![0;1 + max_x as usize]).collect::<Vec<Vec<i32>>>();
 
-    for line in lines.iter().filter(|line| line.x1 == line.x2 || line.y1 == line.y2){
+    // For each line, find the points and add them to the map
+    for line in lines.iter(){
         for point in calculate_line(&line){
-            *vent_map_1.get_mut(point.1).unwrap().get_mut(point.0).unwrap() += 1;
+            // Part 2
+            *vent_map_2.get_mut(point.1).unwrap().get_mut(point.0).unwrap() += 1;
+
+            // Part 1, only add points which aren't diagonal
+            if line.x1 == line.x2 || line.y1 == line.y2{
+                *vent_map_1.get_mut(point.1).unwrap().get_mut(point.0).unwrap() += 1;
+            }
         }
     }
+    
 
     let part_1 = vent_map_1.iter().flatten().filter(|val| val >= &&2).count();
-
-    
-    let part_2 = "B";
+    let part_2 = vent_map_2.iter().flatten().filter(|val| val >= &&2).count();
 
 
     let duration = Instant::now() - time_before;
@@ -59,39 +70,26 @@ pub fn day_5_main() -> Answer{
     Answer{day:4, part_1:part_1.to_string(), part_2:part_2.to_string(), duration:duration}
 }
 
+// Finds all of the points on the line
 fn calculate_line(line:&Line) -> Vec<(usize,usize)>{
     let mut out_line:Vec<(usize,usize)> = vec![];
 
-    if line.x1 - line.x2 != 0{
-        let mut x = line.x1;
-        let x_step = if line.x1 > line.x2 {-1} else {1};
-        let mut counter = 0;
+    // Get the gradient of each axis
+    let x_gradient = (line.x2 - line.x1)/((line.x2 - line.x1).abs().max(1));
+    let y_gradient = (line.y2 - line.y1)/((line.y2 - line.y1).abs().max(1));
 
-        while x != line.x2 + x_step{
 
-            out_line.push((x as usize,(line.y1 + (((line.y2 - line.y1)*counter)/(line.x2-line.x1))) as usize));
+    let mut x = line.x1;
+    let mut y = line.y1;
 
-            x += x_step;
-            counter += 1;
-        }
+    // Loop until we get to the other end and add a point as we go
+    while x != line.x2 || y != line.y2{
+        out_line.push((x as usize,y as usize));
+        x += x_gradient;
+        y += y_gradient;
     }
+    out_line.push((x as usize,y as usize));
 
-    if line.y1 - line.y2 != 0{
-        let mut y = line.y1;
-        let y_step = if line.y1 > line.y2 {-1} else {1};
-        let mut counter = 0;
-
-        while y != line.y2 + y_step{
-
-            out_line.push(((line.x1 + (((line.x2 - line.x1)*counter)/(line.y2-line.y1))) as usize,y as usize));
-
-            y += y_step;
-            counter += 1;
-        }
-    }
-    
-    out_line.sort();
-    out_line.dedup_by(|a,b| a.0 == b.0 && a.1 == b.1);
     out_line
 }
 
