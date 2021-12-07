d519f913a3ea98ca5c36526d201259efcca164a1
Samuel Gregson <55996456+Zacxalot@users.noreply.github.com>
2021-12-07 16:40:33 +0000

    Day 7 Part 1 and 2 a little bit slow IMO, might be ok.
    Updated README too.

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use days::day_3::day_3::day_3_main;
 use days::day_4::day_4::day_4_main;
 use days::day_5::day_5::day_5_main;
 use days::day_6::day_6::day_6_main;
+use days::day_7::day_7::day_7_main;
 
 use prettytable::{Table, Row, row, cell};
 
@@ -44,6 +45,7 @@ fn main() {
     answers.push(day_4_main());
     answers.push(day_5_main());
     answers.push(day_6_main());
+    answers.push(day_7_main());
 
     for answer in &answers{
         table.add_row(answer.to_row());
