fb9ab984057a0554ec44001c730dd74d5428cf4f
Samuel Gregson <55996456+Zacxalot@users.noreply.github.com>
2021-12-08 14:47:32 +0000

    Added comments for day 8 and updated readme

--- src/days/day_8/day_8.rs
+++ src/days/day_8/day_8.rs
@@ -6,7 +6,9 @@ use crate::Answer;
 pub fn day_8_main() -> Answer{
     let time_before = Instant::now();
 
-    // Get our 2d vec of strings
+    // Gets a vec of each line
+    // Each digit on each line is converted to a u8 using digit_chars_to_num
+    // This value is tupled with the number of ones it has in it
     let contents = fs::read_to_string("src/days/day_8/input1.txt")
                       .unwrap()
                       .split("\n")
@@ -17,10 +19,12 @@ pub fn day_8_main() -> Answer{
                                            .collect::<Vec<(u8,u8)>>())
                       .collect::<Vec<Vec<(u8,u8)>>>();
 
+    // Iterates through the list and counts how many output values "lengths" (Now the number of ones in the number) equal 2, 3, 4 or 7
     let part_1 = contents.iter()
                          .map(|line| line[10..14].iter().filter(|(_,ones)| [2,3,4,7].contains(ones)).count())
                          .sum::<usize>();
 
+    // Deduces the values of each line and sums their final value
     let part_2 = contents.iter().map(|digits| deduce_values(digits)).sum::<u32>();
 
     let duration = Instant::now() - time_before;
@@ -30,21 +34,36 @@ pub fn day_8_main() -> Answer{
 
 fn deduce_values(digits:&Vec<(u8,u8)>) -> u32{
     let mut values:[u8; 10] = [0;10];
-    
+    // Get all of the values we know because of their length
     values[1] = digits.iter().find(|(_,ones)| ones == &2).unwrap().0;
     values[4] = digits.iter().find(|(_,ones)| ones == &4).unwrap().0;
     values[7] = digits.iter().find(|(_,ones)| ones == &3).unwrap().0;
     values[8] = digits.iter().find(|(_,ones)| ones == &7).unwrap().0;
+
+    // 3 has 5 ones and has 3 bits different from 1
     values[3] = digits.iter().find(|(val,ones)| ones == &5 && (val ^ values[1]).count_ones() == 3).unwrap().0;
+    
+    // 9 has 6 digits and one bit different to 3
     values[9] = digits.iter().find(|(val,ones)| ones == &6 && (val ^ values[3]).count_ones() == 1).unwrap().0;
+    
+    // 5 has 5 bits, 1 bit different to 9 and isn't the same value as 3
     values[5] = digits.iter().find(|(val,ones)| ones == &5 && (val ^ values[9]).count_ones() == 1 && val != &values[3]).unwrap().0;
+
+    // 2 has 5 bits and isn't 3 or 5
     values[2] = digits.iter().find(|(val,ones)| ones == &5 && val != &values[3] && val != &values[5]).unwrap().0;
+
+    // c is the bit shared by 1 and 2
     let c = values[2] & values[1];
+
+    // 6 has 6 bits and none of them are the same as c
     values[6] = digits.iter().find(|(val,ones)| ones == &6 && val & c == 0).unwrap().0;
+
+    // 0 is 6 digits and not 6 or 9
     values[0] = digits.iter().find(|(val,ones)| ones == &6 && val != &values[9] && val != &values[6]).unwrap().0;
 
     let mut digit_string = "".to_string();
 
+    // Loop through the output digits and find their value from the values array
     for (digit,_) in digits[10..14].iter(){
         digit_string += &values.iter().enumerate().find(|(_, val)| val == &digit).unwrap().0.to_string();
     }
@@ -52,6 +71,8 @@ fn deduce_values(digits:&Vec<(u8,u8)>) -> u32{
     digit_string.parse::<u32>().unwrap()
 }
 
+
+// Assigns each letter to a bit, makes bitwise operations possible
 fn digit_chars_to_num(line:&str) -> u8{
     let mut out:u8 = 0;
 
